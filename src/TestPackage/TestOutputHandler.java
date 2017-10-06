a6e5c07c1ced3c2581fde0524ac31f83035d1554
Tony <tony_tamer@hotmail.com>
2017-10-06 16:04:57 -0400

    testOutputHandler pass

---
+++ src/TestPackage/TestOutputHandler.java
@@ -0,0 +1,35 @@
+package TestPackage;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import server.logic.handler.OutputHandler;
+import server.logic.handler.model.Output;
+
+public class TestOutputHandler {
+	
+	OutputHandler outputHand;
+
+	@Before
+	public void setUp() throws Exception {
+		outputHand = new OutputHandler();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testCreateUser() {
+		Output out = new Output("Your input should in this format:'username,password'", 4);
+		assertEquals(out.toString(), outputHand.createUser("Antony,password").toString());
+		Output out1 = new Output("Success!", 2);
+		assertEquals(out1.toString(), outputHand.createUser("tony.tamer@carleton.ca, MyPass").toString());
+		Output out2 = new Output("The User Already Exists!", 2);
+		assertEquals(out2.toString(), outputHand.createUser("tony.tamer@carleton.ca, MyPass").toString());
+	}
+
+}
---
+++ src/server/logic/handler/OutputHandler.java
@@ -0,0 +1,52 @@
+package server.logic.handler;
+
+import java.util.Date;
+
+import server.logic.handler.model.Output;
+import server.logic.tables.FeeTable;
+import server.logic.tables.ItemTable;
+import server.logic.tables.LoanTable;
+import server.logic.tables.TitleTable;
+import server.logic.tables.UserTable;
+import utilities.Config;
+
+public class OutputHandler {
+	public static final int WAITING = 0;
+	public static final int FINISHWAITING=1;
+    public static final int CLERK = 2;
+    public static final int USER = 3;
+    public static final int CREATEUSER=4;
+    public static final int CREATETITLE=5;
+    public static final int CREATEITEM=6;
+    public static final int DELETEUSER=7;
+    public static final int DELETETITLE=8;
+    public static final int DELETEITEM=9;
+    public static final int BORROW=10;
+    public static final int RENEW=11;
+    public static final int RETURN=12;
+    public static final int PAYFINE=13;
+    public static final int CLERKLOGIN=14;
+    public static final int USERLOGIN=15;
+
+	public Output createUser(String input) {
+		Output output=new Output("",0);
+		String[] strArray = null;   
+        strArray = input.split(",");
+        boolean email=strArray[0].contains("@");
+        Object result="";
+        if(strArray.length!=2 || email!=true){
+        	output.setOutput("Your input should in this format:'username,password'");
+        	output.setState(CREATEUSER);
+        }else{
+        	result=UserTable.getInstance().createuser(strArray[0], strArray[1]);
+        	if(result.equals(true)){
+        		output.setOutput("Success!");
+        	}else{
+        		output.setOutput("The User Already Exists!");
+        	}
+        	output.setState(CLERK);
+        }
+		return output;
+	}
+
+}
