252ac15ca314f13c6d4c961a0bf937dc50b2ba44
Tony <tony_tamer@hotmail.com>
2017-10-08 20:55:09 -0400

    test user display from monitor system

--- src/SystemMonitor/MonitorSystem.java
+++ src/SystemMonitor/MonitorSystem.java
@@ -1,6 +1,7 @@
 package SystemMonitor;
 
 import server.logic.tables.TitleTable;
+import server.logic.tables.UserTable;
 
 
 public class MonitorSystem {
@@ -17,14 +18,22 @@ public class MonitorSystem {
 		return result;
 	}
 	
-//	public void display() {
-//		UserTable users = UserTable.getInstance();
-//		
-//	}
+	public String userDisplay() {
+		UserTable users = UserTable.getInstance();
+		String result = "";
+		System.out.println("Here is a list of all users:");
+		for(int i = 0; i<users.userList.size(); i++) {
+//			System.out.println(titles.titleList.get(i).toString());
+			result = result + users.userList.get(i).toString() + "\n";
+		}
+		System.out.println(result);
+		return result;
+		
+	}
 	
 //	public static void main(String [] args) {
 //		MonitorSystem sys = new MonitorSystem();
-//		sys.titleDisplay();
+//		sys.userDisplay();
 //	}
 
 }
--- src/TestPackage/TestMonitorSystem.java
+++ src/TestPackage/TestMonitorSystem.java
@@ -26,5 +26,11 @@ public class TestMonitorSystem {
 		assertEquals("[9781442668584,By the grace of God]\n[9781442616899,Dante's lyric poetry ]\n[9781442667181,Courtesy lost]\n[9781611687910,Writing for justice]\n[9781317594277,The act in context]\n[1234567890123,The new Title]\n", sys.titleDisplay());
 		assertNotNull(sys.titleDisplay());
 	}
+	
+	@Test
+	public void testUserDisplay() {
+		assertEquals("[0,Zhibo@carleton.ca,Zhibo]\n[1,Yu@carleton.ca,Yu]\n[2,Michelle@carleton.ca,Michelle]\n[3,Kevin@carleton.ca,Kevin]\n[4,Sun@carleton.ca,Sun]\n", sys.userDisplay());
+		assertNotNull(sys.userDisplay());
+	}
 
 }
