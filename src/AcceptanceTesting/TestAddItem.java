7ed93c9398e6bf191a2285306891ac3e55140f27
Tony <tony_tamer@hotmail.com>
2017-10-10 20:23:17 -0400

    testAddItem acc. test

--- src/AcceptanceTesting/TestAddItem.java
+++ src/AcceptanceTesting/TestAddItem.java
@@ -12,22 +12,21 @@ import server.logic.handler.model.Output;
 import server.logic.handler.model.ServerOutput;
 import utilities.Config;
 
-public class TestAddTitle {
-	public static String INPUT;
+public class TestAddItem {
+	public static final String INPUT = "clerk";
+	public static final String CREATEITEM = "create item";
 	public static String CREATETITLE;
-	
 	@Before
 	public void setUp() throws Exception {
-		INPUT = "clerk";
 		CREATETITLE  = "create title";
 	}
 
 	@After
 	public void tearDown() throws Exception {
 	}
-	
+
 	@Test
-	public void testAddTitleSuccess() {
+	public void testAddItemSuccessfully() {
 		InputHandler input = new InputHandler();
 		ServerOutput out = new ServerOutput("", 0);
 		OutputHandler output = new OutputHandler();
@@ -38,22 +37,22 @@ public class TestAddTitle {
 		if(out.getState() == InputHandler.CLERKLOGIN) {
 			output1 = output.clerkLogin(Config.CLERK_PASSWORD);
 			if(output1.getState() == OutputHandler.CLERK) {
-				out = input.processInput(CREATETITLE, output1.getState());
+				out = input.processInput(CREATEITEM, output1.getState());
 			}else{
 				fail("");
 			}
 			System.out.println(out.getOutput());
 			System.out.println(output1.getState());
 		
-			if(out.getState() == InputHandler.CREATETITLE && out.getOutput() != null) {
-				output1 = output.createTitle("1222111444554,The COMP 4004");
+			if(out.getState() == InputHandler.CREATEITEM && out.getOutput() != null) {
+				output1 = output.createItem("9781442616899");
 			}
 			assertEquals("Success!", output1.getOutput());
 		}
 	}
 	
 	@Test
-	public void testAddTitleThatAlreadyExists() {
+	public void testAddItemThatDoesntExist() {
 		InputHandler input = new InputHandler();
 		ServerOutput out = new ServerOutput("", 0);
 		OutputHandler output = new OutputHandler();
@@ -64,22 +63,43 @@ public class TestAddTitle {
 		if(out.getState() == InputHandler.CLERKLOGIN) {
 			output1 = output.clerkLogin(Config.CLERK_PASSWORD);
 			if(output1.getState() == OutputHandler.CLERK) {
-				out = input.processInput(CREATETITLE, output1.getState());
+				out = input.processInput(CREATEITEM, output1.getState());
 			}else{
 				fail("");
 			}
 			System.out.println(out.getOutput());
 			System.out.println(output1.getState());
 		
-			if(out.getState() == InputHandler.CREATETITLE && out.getOutput() != null) {
-				output1 = output.createTitle("9781442668584,This should already Exists");
+			if(out.getState() == InputHandler.CREATEITEM && out.getOutput() != null) {
+				output1 = output.createItem("1234565432121");
 			}
-			assertEquals("The Title Already Exists!", output1.getOutput());
+			if(output1.getState() == InputHandler.CREATETITLE) {
+				if(input.processInput("", InputHandler.WAITING) == null) {
+					fail("");
+				}
+				
+				out = input.processInput("create title", InputHandler.CLERK);
+				if(out.getState() == InputHandler.CREATETITLE && out.getOutput() != null) {
+					output1 = output.createTitle("1222111444557,The COMP 4004");
+				}
+			}
+			if(output1.getState() == OutputHandler.CLERK) {
+				out = input.processInput(CREATEITEM, output1.getState());
+			}else{
+				fail("");
+			}
+			System.out.println(out.getOutput());
+			System.out.println(output1.getState());
+		
+			if(out.getState() == InputHandler.CREATEITEM && out.getOutput() != null) {
+				output1 = output.createItem("1222111444557");
+				assertEquals("Success!", output1.getOutput());
+			}
 		}
 	}
 	
 	@Test
-	public void testAddTitleIncorrectly() {
+	public void addItemWithIncorrectISBN() {
 		InputHandler input = new InputHandler();
 		ServerOutput out = new ServerOutput("", 0);
 		OutputHandler output = new OutputHandler();
@@ -90,17 +110,17 @@ public class TestAddTitle {
 		if(out.getState() == InputHandler.CLERKLOGIN) {
 			output1 = output.clerkLogin(Config.CLERK_PASSWORD);
 			if(output1.getState() == OutputHandler.CLERK) {
-				out = input.processInput(CREATETITLE, output1.getState());
+				out = input.processInput(CREATEITEM, output1.getState());
 			}else{
 				fail("");
 			}
 			System.out.println(out.getOutput());
 			System.out.println(output1.getState());
 		
-			if(out.getState() == InputHandler.CREATETITLE && out.getOutput() != null) {
-				output1 = output.createTitle("The Good Man,1234324565431");
+			if(out.getState() == InputHandler.CREATEITEM && out.getOutput() != null) {
+				output1 = output.createItem("97814426168");
 			}
-			assertEquals("Your input should be in this format:'ISBN,title',ISBN should be a 13-digit number", output1.getOutput());
+			assertEquals("Your input should be in this format:'ISBN',ISBN should be a 13-digit number", output1.getOutput());
 		}
 	}
 
